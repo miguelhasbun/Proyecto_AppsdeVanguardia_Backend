67bba81be4f25d9a6299b170b7602b1b7ae7bd22
unknown <miguelneptaly@gmail.com>
2020-03-10 23:20:37 -0600

    conexión a Mongo Atlas lista

--- Backend/src/Models/UserModel.js
+++ Backend/src/Models/UserModel.js
@@ -1,6 +1,10 @@
 var mongoose= require("mongoose");
 
-mongoose.connect("mongodb://localhost/ProyectoVanguardia", {useNewUrlParser: true,useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost/ProyectoVanguardia", {useNewUrlParser: true,useUnifiedTopology: true});
+
+mongoose.connect('mongodb+srv://sa:1234@cluster0-pyexl.azure.mongodb.net/test', {useNewUrlParser:true, useUnifiedTopology: true} ).then( ()=> {
+    console.log(`La Conexión a Base de Datos: es Correcta!!`);
+} );   
 
 const userSchemaJson = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
@@ -9,4 +13,5 @@ const userSchemaJson = mongoose.Schema({
     clave: {type: String, required: true},
     img: {type: String, required: true}
 });
+
 module.exports= mongoose.model('users', userSchemaJson);
