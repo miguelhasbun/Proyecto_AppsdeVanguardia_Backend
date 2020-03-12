acf00ed53a2e9c151ca2988c8c4df4b183c73685
Andy Escobar <andyescobar007@gmail.com>
2020-03-12 10:50:45 -0600

    agregando configuraciones necesarias

--- Backend/src/Services/config.js
+++ Backend/src/Services/config.js
@@ -1,8 +1,6 @@
 module.exports = {
-	'DATABASE': 'localhost:3000/faceRecognition',
-	'SECRET':	'thisisasupersecretpasswordthingy',
-    'FACE_API_KEY': '',
-    'FACE_API_HOST': 'westus.api.cognitive.microsoft.com',
+    'FACE_API_KEY': 'cf718d5849d14d7880bf62eaf6cf3f6b',
+    'FACE_API_HOST': 'face-loginangular.cognitiveservices.azure.com',
     'FACE_API_PATH_VERIFY': '/face/v1.0/verify',
     'FACE_API_PATH_DETECT': '/face/v1.0/detect?returnFaceId=true',
     'FACE_API_CONFIDENCE_TRESHOLD': 0.75
